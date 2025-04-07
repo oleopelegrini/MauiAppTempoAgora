3913cbfd48a5def7ebcc9b9a83c6ce3263fb6883
231284 <231284@CPD03-M07>
2025-04-07 11:35:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/Models/Tempo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppTempoAgora.Models
+{
+    internal class Tempo
+    {
+        public double? lon {  get; set; }
+        public double? lat { get; set; }
+        public double? tempo_min { get; set; }
+        public double? tempo_max { get; set; }
+        public double? visibility { get; set; }
+        public double? speed { get; set; }
+        public double? main { get; set; }
+        public double? description { get; set; }
+        public double? sunrise { get; set; }
+    }
+}
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+using MauiAppTempoAgora.Models;
+
+namespace MauiAppTempoAgora.Services
+{
+    internal class DataService
+    {
+
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "6135072afe7f6cec1537d5cb08a5a1a2";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime Time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        tempo_min = (double)rascunho["main"]["tempo_min"],
+                        tempo_max = (double)rascunho["wind"]["speed"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    }; // Fecha obj do tempo.
+                } // Fecha if se o status do servidor foi de sucesso
+            } // Fecha laço using
+
+            return t;
+        }
+    }
+}
